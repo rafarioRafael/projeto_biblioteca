58b10f0135a217724b84fb4396996e474fd65bb6
Rafael <rafael.miranda079@gmail.com>
2023-11-02 21:48:49 -0300

    atualizando db

--- script_db/novo_biblioteca.sql
+++ script_db/novo_biblioteca.sql
@@ -1,92 +1,32 @@
--- MySQL Workbench Forward Engineering
+create database biblioteca;
+use biblioteca;
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+create table if not exists tbl_clientes(
+	id_clientes int auto_increment primary key,
+    nome varchar(255) not null,
+    sobrenome varchar(255) not null,
+    email varchar(100) not null,
+    endereco varchar(255) not null,
+    dataNascimento date not null
+);
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema biblioteca
--- -----------------------------------------------------
+create table if not exists tbl_livros(
+	id_livro int auto_increment primary key,
+    titulo VARCHAR(255) NOT NULL,
+    alugadoPor VARCHAR(45) NULL DEFAULT NULL,
+    data_inicial DATE NULL DEFAULT NULL,
+    data_final DATE NULL DEFAULT NULL
+);
 
--- -----------------------------------------------------
--- Schema biblioteca
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `biblioteca` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `biblioteca` ;
+create table if not exists tbl_estoque(
+	id_estoque INT NOT NULL AUTO_INCREMENT primary key,
+    titulo VARCHAR(255) NULL DEFAULT NULL,
+    autor VARCHAR(255) NULL DEFAULT NULL,
+    editora VARCHAR(255) NULL DEFAULT NULL,
+    genero VARCHAR(255) NULL DEFAULT NULL,
+    isbn VARCHAR(45) NULL DEFAULT NULL,
+    ano_publicacao VARCHAR(4) NULL DEFAULT NULL,
+    qtd INT NULL DEFAULT NULL
+);
 
--- -----------------------------------------------------
--- Table `biblioteca`.`tbl_clientes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `biblioteca`.`tbl_clientes` (
-  `id_clientes` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(255) NOT NULL,
-  `sobrenome` VARCHAR(255) NULL DEFAULT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `endereco` VARCHAR(255) NOT NULL,
-  `dataNascimento` DATE NULL DEFAULT NULL,
-  PRIMARY KEY (`id_clientes`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 22
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `biblioteca`.`tbl_livros`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `biblioteca`.`tbl_livros` (
-  `id_livro` INT NOT NULL AUTO_INCREMENT,
-  `titulo` VARCHAR(255) NOT NULL,
-  `alugadoPor` VARCHAR(45) NULL DEFAULT NULL,
-  `data_inicial` DATE NULL DEFAULT NULL,
-  `data_final` DATE NULL DEFAULT NULL,
-  PRIMARY KEY (`id_livro`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 62
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `biblioteca`.`tbl_estoque`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `biblioteca`.`tbl_estoque` (
-  `id_estoque` INT NOT NULL AUTO_INCREMENT,
-  `titulo` VARCHAR(255) NULL DEFAULT NULL,
-  `autor` VARCHAR(255) NULL DEFAULT NULL,
-  `editora` VARCHAR(255) NULL DEFAULT NULL,
-  `genero` VARCHAR(255) NULL DEFAULT NULL,
-  `isbn` VARCHAR(45) NULL DEFAULT NULL,
-  `ano_publicacao` VARCHAR(4) NULL DEFAULT NULL,
-  `qtd` INT NULL DEFAULT NULL,
-  `id_livro` INT NULL DEFAULT NULL,
-  PRIMARY KEY (`id_estoque`),
-  INDEX `id_livro` (`id_livro` ASC) VISIBLE,
-  CONSTRAINT `tbl_estoque_ibfk_1`
-    FOREIGN KEY (`id_livro`)
-    REFERENCES `biblioteca`.`tbl_livros` (`id_livro`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 20
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `biblioteca`.`tbl_funcionarios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `biblioteca`.`tbl_funcionarios` (
-  `id_funcionario` SMALLINT NOT NULL AUTO_INCREMENT,
-  `nomecompleto` VARCHAR(255) NOT NULL,
-  `senha` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id_funcionario`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+alter user 'root'@'localhost' identified with mysql_native_password by 'root';
---
+++ server/dist/controllers/atribuir.controller.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.atribuirLivro = void 0;
+const connection_1 = __importDefault(require("../db/connection"));
+const atribuirLivro = (req, res) => {
+    const { idLivro, idPessoa } = req.params;
+    // Primeiro, você pode verificar se há estoque disponível antes de atribuir o livro
+    connection_1.default.query('SELECT qtd FROM tbl_estoque WHERE id_livro = ?', idLivro, (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: 'Erro ao verificar o estoque' });
+        }
+        const quantidadeEmEstoque = result[0].quantidade;
+        if (quantidadeEmEstoque <= 0) {
+            return res.status(400).json({ error: 'Estoque esgotado' });
+        }
+        // Se houver estoque disponível, você pode atribuir o livro
+        connection_1.default.query('INSERT INTO cliente_livro (id_clientes, id_livros,is_active) VALUES (?, ?, ?)', [idPessoa, idLivro, true], (err, data) => {
+            if (err) {
+                return res.status(500).json({ error: 'Erro ao atribuir o livro' });
+            }
+            // Agora você pode atualizar a quantidade no estoque
+            connection_1.default.query('UPDATE tbl_estoque SET quantidade = quantidade - 1 WHERE id_livro = ?', idLivro, (err, data) => {
+                if (err) {
+                    return res.status(500).json({ error: 'Erro ao atualizar o estoque' });
+                }
+                res.json({ message: 'Livro atribuído com sucesso' });
+            });
+        });
+    });
+};
+exports.atribuirLivro = atribuirLivro;
